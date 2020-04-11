2e588b944228dfb94d4d79b4a603f78f04779b58
ernestkoko <ernesteferetin@yahoo.com>
2020-04-11 12:50:36 +0100

    Added error handling not to mess up the database

--- src/main/java/com/wiredbrain/friends/controller/FriendController.java
+++ src/main/java/com/wiredbrain/friends/controller/FriendController.java
@@ -3,6 +3,8 @@ package com.wiredbrain.friends.controller;
 import com.wiredbrain.friends.model.Friend;
 import com.wiredbrain.friends.service.FriendService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
@@ -27,8 +29,12 @@ public class FriendController {
         return friendService.findAll();
     }
     @PutMapping("/friend")
-    Friend update(@RequestBody Friend friend){
-        return friendService.save(friend);
+    ResponseEntity<Friend> update(@RequestBody Friend friend){ //changed the return type to handle any error
+        if (friendService.findById(friend.getId()).isPresent())
+            return  new ResponseEntity(friendService.save(friend), HttpStatus.OK);
+         else
+        return  new ResponseEntity(friend, HttpStatus.BAD_REQUEST);
+         //if the friend does not exist it sends bad request and does not change the existing friend
     }
 
     @DeleteMapping("/friend/{id}")
