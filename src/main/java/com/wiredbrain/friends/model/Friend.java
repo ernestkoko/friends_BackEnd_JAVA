9ff9c58d56ad02003d89a5e39aa999d10193b069
ernestkoko <ernesteferetin@yahoo.com>
2020-04-11 11:57:50 +0100

    Added Json Annotations to the Friend class and generated the getters and setters methods

--- src/main/java/com/wiredbrain/friends/model/Friend.java
+++ src/main/java/com/wiredbrain/friends/model/Friend.java
@@ -1,5 +1,8 @@
 package com.wiredbrain.friends.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -10,8 +13,29 @@ public class Friend {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @JsonProperty("first-name")
     private String firstName;
+    @JsonProperty("last-name")
     private String lastName;
+    int age;
+    @JsonIgnore
+    boolean married;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isMarried() {
+        return married;
+    }
+
+    public void setMarried(boolean married) {
+        this.married = married;
+    }
 
     public int getId() {
         return id;
